0afd5204c6b8a4a2dfb1ccabefff77117557cb57
Alexey <alex.netjournal@gmail.com>
2021-03-19 17:54:29 +0300

    pagination v1

--- src/main/webapp/src/components/Phones.js
+++ src/main/webapp/src/components/Phones.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from "axios";
-import {Card} from 'antd'
+import {Card, Pagination} from 'antd'
 import {ShoppingCartOutlined} from '@ant-design/icons'
 import '../App.css'
 
@@ -9,9 +9,30 @@ const {Meta} = Card;
 
 class Phones extends React.Component {
 
-    state = {
-        phones: []
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            phones: [],
+            minValue: 0,
+            maxValue: 2
+        };
     }
+
+
+    handleChange = value => {
+        (value <= 1) ?
+            this.setState({
+                minValue: 0,
+                maxValue: 2
+            })
+            :
+            this.setState({
+                minValue: this.state.maxValue,
+                maxValue: value * 2
+            });
+    };
+
 
     componentDidMount() {
         axios.get('/api/products').then(response => {
@@ -21,25 +42,39 @@ class Phones extends React.Component {
     }
 
     render() {
+
+        const renderPhones = this.state.phones.slice(this.state.minValue, this.state.maxValue).map((phone) => {
+            return <Card bordered={true}
+                         className="myCard">
+                <a href="/#">
+                    <Meta
+                        title={phone.name}
+                        description={phone.description}
+                    />
+                </a>
+                <div className="priceCart">
+                    <a href="/#" className="price">{phone.price} RUB</a>
+                    <a href="/#" className="cart">
+                        <ShoppingCartOutlined/>
+                    </a>
+                </div>
+            </Card>
+        });
+
+
         return (
-            <div className="cardWrapper">
-                {this.state.phones.map(phone =>
-                    <Card bordered={true}
-                          className="myCard">
-                        <a href="/#">
-                            <Meta
-                                title={phone.name}
-                                description={phone.description}
-                            />
-                        </a>
-                        <div className="priceCart">
-                            <a href="/#" className="price">{phone.price} RUB</a>
-                                <a href="/#" className="cart">
-                                    <ShoppingCartOutlined />
-                                </a>
-                        </div>
-                    </Card>
-                )}
+            <div>
+                <div className="cardWrapper">
+                    {renderPhones}
+                </div>
+
+                <Pagination
+                    className = "pagination"
+                    defaultCurrent={1}
+                    defaultPageSize={this.state.maxValue}
+                    onChange={this.handleChange}
+                    total={this.state.phones.length}
+                />
             </div>
         )
     }
