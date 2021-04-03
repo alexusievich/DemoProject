f9d1fb3e69753ade643710983254da67e156c913
Alexey <alex.netjournal@gmail.com>
2021-04-03 18:25:06 +0300

    recent added

--- src/main/webapp/src/components/MainPage.js
+++ src/main/webapp/src/components/MainPage.js
@@ -1,10 +1,95 @@
 import banner from "../images/banner.jpg";
 import React from "react";
+import axios from "axios";
+import {Card} from "antd";
+import {Link} from "react-router-dom";
+import {ShoppingCartOutlined} from "@ant-design/icons";
+import '../styles/MainPage.css'
 
-function MainPage() {
-    return (
-        <img src={banner} alt="main banner" width="100%"/>
-    )
+const {Meta} = Card;
+
+class MainPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            phones: [],
+        };
+    }
+
+    componentDidMount() {
+
+        let id1 = '';
+        let id2 = '';
+        let str = '';
+
+        document.cookie.lastIndexOf(' ') === -1 ? id1 = document.cookie
+            : id1 = document.cookie.slice(document.cookie.lastIndexOf(' ') + 1);
+        str = document.cookie.substring(0, document.cookie.lastIndexOf(' '));
+        str.lastIndexOf(' ') === -1 ? id2 = str : id2 = str.slice(str.lastIndexOf(' ') + 1);
+
+        if (id2 === id1) {
+            document.cookie = document.cookie.substring(0, document.cookie.lastIndexOf(id2));
+            document.cookie = document.cookie.substring(0, document.cookie.lastIndexOf(id2));
+            id2 === document.cookie ? id2 ='' : id2 = document.cookie.substring(document.cookie.lastIndexOf(' ') + 1)
+        }
+
+        if (id1 !== '' ) {axios.get('/api/products/' + id1).then(response => {
+            const phone = response.data;
+            this.setState(prevState => ({
+                phones: [...prevState.phones, phone]
+            }))
+        })}
+
+        if ( id2 !== '') {axios.get('/api/products/' +  id2).then(response => {
+            const phone = response.data;
+            this.setState(prevState => ({
+                phones: [...prevState.phones, phone]
+            }))
+        })}
+
+    };
+
+    render() {
+
+        const renderPhones = (this.state.phones.map(phone => {
+                return (
+                    <Card hoverable
+                          key={phone.id}
+                          className="recentCards"
+                          cover={
+                              <Link to={"/phones/" + phone.id}>
+                                  <img alt={phone.name}
+                                       src={phone.img}
+                                  />
+                              </Link>
+                          }>
+                        <Meta
+                            title={phone.name}
+                            description={phone.config}
+                        />
+                        <div className="priceCart">
+                            <div className="price">{phone.price} RUB</div>
+                            <a href="/#" className="cart">
+                                <ShoppingCartOutlined/>
+                            </a>
+                        </div>
+                    </Card>
+                )
+            })
+        )
+
+        return (
+            <div>
+                <img src={banner} alt="main banner" width="100%"/>
+                <div className="recentProducts">
+                    {this.state.phones.length > 0 && <div className="text">Recently viewed</div>}
+                    <div className="wrapper">{renderPhones}</div>
+                </div>
+            </div>
+
+        )
+    }
 }
 
 export default MainPage
--- src/main/webapp/src/components/PhoneDetails.js
+++ src/main/webapp/src/components/PhoneDetails.js
@@ -20,13 +20,16 @@ class PhoneDetails extends React.Component {
         axios.get('/api/products/' + id).then(response => {
             const phoneDetails = response.data;
             this.setState({phoneDetails});
-        }).catch( error => {
+        }).catch(error => {
             this.props.history.push("/404");
         })
     };
 
 
     render() {
+
+        this.state.phoneDetails ? document.cookie += " " + this.state.phoneDetails.id: document.cookie += '';
+
         const renderImages = this.state.phoneDetails?.images.sort((a, b) => a.id - b.id).map(image => {
 
             return (
@@ -58,38 +61,38 @@ class PhoneDetails extends React.Component {
         return (
             <div>
                 {this.state.phoneDetails &&
-                    <div className="phonedetails">
-                        <div className="images">
-                            <Carousel autoplay>
-                                {renderImages}
-                            </Carousel>
+                <div className="phonedetails">
+                    <div className="images">
+                        <Carousel autoplay>
+                            {renderImages}
+                        </Carousel>
+                    </div>
+                    <div className="information">
+                        <div className="name">
+                            {this.state.phoneDetails?.name} {this.state.phoneDetails?.config}
                         </div>
-                        <div className="information">
-                            <div className="name">
-                                {this.state.phoneDetails?.name} {this.state.phoneDetails?.config}
-                            </div>
-                            <div className="rating">
-                                {this.state.phoneDetails?.rating} <StarFilled style={{color: "#1890ff"}}/>
-                            </div>
-                            <div className="description">
-                                {this.state.phoneDetails?.description}
-                            </div>
-                            <div className="pricee">
-                                {this.state.phoneDetails?.price} RUB
-                            </div>
-                            <div className="cartt">
-                                <a href="/#">
-                                    <Button type="primary" shape="round" icon={<ShoppingCartOutlined/>} size="large">
-                                        Add to cart
-                                    </Button>
-                                </a>
-                            </div>
+                        <div className="rating">
+                            {this.state.phoneDetails?.rating} <StarFilled style={{color: "#1890ff"}}/>
                         </div>
-                        <div className="techspecs">
-                            <div className="techspectitle">Technical Specifications</div>
-                            {renderTechSpecs}
+                        <div className="description">
+                            {this.state.phoneDetails?.description}
+                        </div>
+                        <div className="pricee">
+                            {this.state.phoneDetails?.price} RUB
+                        </div>
+                        <div className="cartt">
+                            <a href="/#">
+                                <Button type="primary" shape="round" icon={<ShoppingCartOutlined/>} size="large">
+                                    Add to cart
+                                </Button>
+                            </a>
                         </div>
                     </div>
+                    <div className="techspecs">
+                        <div className="techspectitle">Technical Specifications</div>
+                        {renderTechSpecs}
+                    </div>
+                </div>
                 }
 
             </div>
