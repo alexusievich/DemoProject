a5bb9d703d4ecf3b6f3cecb66a7c75c8c70161b2
Alexey <alex.netjournal@gmail.com>
2021-05-18 00:36:37 +0300

    final

--- src/main/webapp/src/components/AppFooter.js
+++ src/main/webapp/src/components/AppFooter.js
@@ -14,8 +14,8 @@ class AppFooter extends React.Component{
                 </ul>
                 <div className="connect"><PhoneOutlined /> Telephone: <a href="tel:+79524635291">+7(952)-463-52-91</a></div>
                 <div className="connect">&copy; 2021 Shop52. All rights reserved.</div>
-                <div className="connect"><SendOutlined /> E-mail: <a href="mailto:shop52@gmail.com">
-                    shop52@gmail.com</a></div>
+                <div className="connect"><SendOutlined /> E-mail: <a href="mailto:shop52NC@gmail.com">
+                    shop52NC@gmail.com</a></div>
             </div>
         )
     }
